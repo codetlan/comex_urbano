3c7e70e7dd92eee2ebb8d018f68c8ed2b96d1a32
waldix <waldix86@gmail.com>
2014-06-26 21:14:48 -0500

    Se sube modulos de admin, videos, galeria, roles, devise

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -9,6 +9,4 @@ class Banner < ActiveRecord::Base
       medium: '300x300>'
   }
 
-  # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
