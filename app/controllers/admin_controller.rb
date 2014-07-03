223ad9dab5e9ee2f3f705d818d5c8c67b04aa631
Salvador Mendo <simiographics@gmail.com>
2014-07-03 01:07:49 -0500

    admin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,12 @@
+class AdminController < ApplicationController
+	layout 'admin'
+
+	def index
+    @categories = Category.all.where('active = ?', 1)
+    @landing_pages = LandingPage.all
+
+    #respond_to do |format|
+     # format.html { redirect_to(landing_pages_path) }
+    #end
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,12 +7,4 @@ class HomeController < ApplicationController
     @videos = Video.all
   end
 
-  def admin
-    @categories = Category.all.where('active = ?', 1)
-    @landing_pages = LandingPage.all
-
-    #respond_to do |format|
-     # format.html { redirect_to(landing_pages_path) }
-    #end
-  end
 end
