f16598003f1ddf9363aaa3db47b2967a813da41f
waldix <waldix86@gmail.com>
2014-07-13 23:11:27 -0500

    Modificacion de migrates

--- db/migrate/20140710222050_add_category_id_to_sections.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoryIdToSections < ActiveRecord::Migration
-  def up
-    add_column :sections, :category_id, :integer
-  end
-
-  def down
-    remove_column :sections, :category_id
-  end
-end
