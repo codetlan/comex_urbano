5c6fefc354bedd9b418f8e3eb2bf259c40c7680a
waldix <waldix86@gmail.com>
2014-07-14 11:30:25 -0500

    Archivos de Configuracion

--- config/initializers/settings.rb
+++
@@ -1,9 +0,0 @@
-##  THESE SETTING SHOULD NOT NEED TO BE SECURE.  IF YOU NEED TO USE SECURE
-##  SETTINGS USE -- config/config.yml AND FOLLOW THE INSTRUCTIONS ON THAT PAGE
-##  TO DEPLOY
-
-require 'yaml'
-
-# Loading variables for Omniauth providers
-
-env = YAML.load_file("#{Rails.root}/config/settings.yml")['amazon_s3'][Rails.env]
