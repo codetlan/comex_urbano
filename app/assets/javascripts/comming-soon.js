733d64dafe74a8b775500722c38b2d9b1f56637a
Salvador Mendo <simiographics@gmail.com>
2014-07-15 00:43:19 -0500

    vista proximamente0

---
+++ app/assets/javascripts/comming-soon.js
@@ -0,0 +1,28 @@
+
+function commingSoon() {
+	var winH = $(window).height(),
+			page = $('.comming-soon'),
+			header = page.find('.comming-soon-header'),
+			footer = page.find('.comming-soon-footer')
+			content = page.find('.comming-soon-content'),
+			contentH = content.outerHeight(),
+			limitHeight = header.outerHeight() + content.outerHeight() + footer.outerHeight();
+
+	console.log(winH,limitHeight);
+
+	if ( winH > limitHeight) {
+		page.height(winH).removeClass('not-fit');
+		content.css('top', winH/2 - contentH/2);
+	} else {
+		content.css('top', '0px');
+		page.addClass('not-fit');
+	}
+};
+
+$(document).ready(function() {
+	commingSoon();
+
+	$(window).resize(function(event) {
+		commingSoon();
+	});
+});
