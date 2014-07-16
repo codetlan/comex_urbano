8ed7c1e09fd9abe976ac7ca74f103273c4b03247
waldix <waldix86@gmail.com>
2014-07-16 12:03:36 -0500

    Correcion de categorias

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to '/admin/categories', notice: 'Category was successfully created.' }
         format.json { render action: 'show', status: :created, location: @category }
       else
         format.html { render action: 'new' }
@@ -52,7 +52,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to '/admin/categories', notice: 'Category was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -66,7 +66,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url }
+      format.html { redirect_to '/admin/categories' }
       format.json { head :no_content }
     end
   end
