73ce8854c4cf84424e0650a4328df53ac27cf8f7
waldix <waldix86@gmail.com>
2014-07-24 19:19:17 -0500

    Cambios de busqueda

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -4,7 +4,7 @@ module TextHelper
 
   def truncate_html(text, max_length = 30, ellipsis = "...")
     tag_free = text.gsub(TAG_PATTERN, '')
-    truncate(tag_free.html_safe, :length => max_length, :omission => ellipsis)
+    truncate(tag_free, :length => max_length, :omission => ellipsis)
   end
 
 end
