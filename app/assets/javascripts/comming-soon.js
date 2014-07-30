fcb858caf32fec2b5a73cd228733f69760366f75
Salvador Mendo <simiographics@gmail.com>
2014-07-29 23:37:57 -0500

    error 404

--- app/assets/javascripts/comming-soon.js
+++ app/assets/javascripts/comming-soon.js
@@ -1,26 +1,50 @@
+var ready;
+var commingSoon;
 
-function commingSoon() {
+commingSoon = function() {
 	var winH = $(window).height(),
 			page = $('.comming-soon'),
 			header = page.find('.comming-soon-header'),
 			footer = page.find('.comming-soon-footer')
-			content = page.find('.comming-soon-content'),
+			content = $('.comming-soon-content'),
 			contentH = content.outerHeight(),
 			limitHeight = header.outerHeight() + content.outerHeight() + footer.outerHeight();
 
+	console.log(content.outerHeight());
+
 	if ( winH > limitHeight) {
 		page.height(winH).removeClass('not-fit');
-		content.css('top', winH/2 - contentH/2);
+		content.css('top', '50%');
+		content.css({
+			top: '50%',
+			marginTop: contentH/2 * -1
+		});
 	} else {
-		content.css('top', '0px');
-		page.addClass('not-fit');
+		content.css({
+			top: '0px',
+			marginTop: '0px'
+		});
+		page.height('auto').addClass('not-fit');
 	}
 };
 
-$(document).ready(function() {
-	commingSoon();
+ready = function() {
+
+	var imageLoaded = function() {
+		commingSoon();
+  }
+
+  $('img').each(function() {
+      var tmpImg = new Image() ;
+      tmpImg.onload = imageLoaded;
+      tmpImg.src = $(this).attr('src') ;
+  });
 
 	$(window).resize(function(event) {
 		commingSoon();
 	});
-});
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
