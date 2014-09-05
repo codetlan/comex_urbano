4f0e54bd3879817643d135112950475209e7665f
waldix <waldix86@gmail.com>
2014-09-05 00:46:55 -0500

    Se corrije error de post

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ ComexUrbano::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  #config.force_ssl = true
+  # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -56,7 +56,7 @@ ComexUrbano::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   #config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = 'http://comex-urbano.com'
+  config.action_controller.asset_host = 'https://urbano-demo.herokuapp.com'
 
 
   # Precompile additional assets.
@@ -84,7 +84,7 @@ ComexUrbano::Application.configure do
 
   config.assets.precompile += %w( ckeditor/* )
 
-  config.action_mailer.default_url_options = { :host => 'comex-urbano.com' }
+  config.action_mailer.default_url_options = { :host => 'urbano-demo.herokuapp.com' }
 
   config.paperclip_defaults = {
       :storage => :s3,
